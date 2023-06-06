af4fd21a15b5f14cbbe27a60a5df08f0aa380701
Quise <darkquise@gmail.com>
2023-06-06 09:08:43 -0400

    added starter for User model

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+userSchema = new mongoose.Schema({
+    username: { 
+        type: String,
+        required: true, 
+        // Check if the 2 below are correct
+        unique: true, 
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: /^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/
+    },
+    // figure out how to do this
+    thoughts: needtofillin,
+    friends: needtofillin
+})
